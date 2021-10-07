fb3064da5130665c2307f18492a1df66a22a7e90
Denis P <denisp.@Deniss-Mac-mini.local>
2021-10-07 16:18:15 +0300

    prototype: issue #1. preparation.Impelement cgen(console app)

---
+++ src/Prototypes/cgen-prototype/CGen-App/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CGen_App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello CGen App!");
+        }
+    }
+}
