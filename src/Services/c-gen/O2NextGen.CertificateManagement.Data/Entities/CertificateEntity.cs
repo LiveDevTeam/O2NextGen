0b34be30b87bdba374d57f6af1beaf27202cd58d
Denis Prokhorchik <live-dev@hotmail.com>
2021-10-28 06:22:17 +0300

    feat: add project O2NextGen.CertificateManagement.Data.csproj

---
+++ src/Services/c-gen/O2NextGen.CertificateManagement.Data/Entities/CertificateEntity.cs
@@ -0,0 +1,8 @@
+namespace O2NextGen.CertificateManagement.Data.Entities
+{
+    public class CertificateEntity
+    {
+        public long Id { get; set; }
+        public string Name { get; set; }
+    }
+}
