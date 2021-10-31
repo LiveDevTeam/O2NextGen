3fc3e133f7688f34f97a18b7895d928e8cbb80e4
Denis Prokhorchik <live-dev@hotmail.com>
2021-10-31 14:33:57 +0300

    devops: update prep-mac.sh

--- src/Database/Prototype/prep-mac.sh
+++ src/Database/Prototype/prep-mac.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
-docker run -e "ACCEPT_EULA=Y" -e "SA_PASSWORD=your@Password" -e "MSSQL_PID=sql" -p 1433:1433 -d mcr.microsoft.com/mssql/server:2017-latest up
-
+# docker run -e "ACCEPT_EULA=Y" -e "SA_PASSWORD=your@Password" -e "MSSQL_PID=sql" -p 1433:1433 -d mcr.microsoft.com/mssql/server:2017-latest up
+sudo docker run -e "ACCEPT_EULA=Y" -e "SA_PASSWORD=your@Password" \
+   -p 1433:1433 --name sql1 -h sql1 \
+   -d \
+   mcr.microsoft.com/mssql/server:2017-latest
