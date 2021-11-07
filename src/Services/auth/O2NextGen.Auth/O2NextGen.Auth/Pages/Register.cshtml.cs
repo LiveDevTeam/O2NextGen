e1db896a4dc5a230ea8ca91ce0b9d914dfaf7afb
Denis Prokhorchik <live-dev@hotmail.com>
2021-11-07 16:11:16 +0300

    issue-80: add register page(o2-auth)

---
+++ src/Services/auth/O2NextGen.Auth/O2NextGen.Auth/Pages/Register.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace O2NextGen.Auth.Pages
+{
+    public class Register : PageModel
+    {
+        public void OnGet()
+        {
+            
+        }
+    }
+}
