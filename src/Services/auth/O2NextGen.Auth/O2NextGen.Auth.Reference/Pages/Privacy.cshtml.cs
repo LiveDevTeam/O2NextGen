1b7d42ef655eaf18ef436b37fb9b02192884db56
Denis Prokhorchik <live-dev@hotmail.com>
2021-11-07 04:32:03 +0300

    issue-80: add simple project(webpp with individual auth)

---
+++ src/Services/auth/O2NextGen.Auth/O2NextGen.Auth.Reference/Pages/Privacy.cshtml.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace O2NextGen.Auth.Reference.Pages
+{
+    public class PrivacyModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
