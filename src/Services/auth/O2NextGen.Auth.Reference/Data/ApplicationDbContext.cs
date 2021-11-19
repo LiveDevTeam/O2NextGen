dc19d2ca047bb92af8d19666a424a9c03dcb342a
Denis Prokhorchik <live-dev@hotmail.com>
2021-11-19 14:23:12 +0300

    chore: move files

--- src/Services/auth/O2NextGen.Auth.Reference/Data/ApplicationDbContext.cs
+++ src/Services/auth/O2NextGen.Auth.Reference/Data/ApplicationDbContext.cs
@@ -1,21 +1,16 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Text;
-using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore;
 
-namespace O2NextGen.Auth.Reference.Data
+namespace O2NextGen.Auth.Sample.Data
 {
     public class ApplicationDbContext : IdentityDbContext
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
-        }
-
-        protected override void OnModelCreating(ModelBuilder builder)
-        {
-            base.OnModelCreating(builder);
         }
     }
 }
