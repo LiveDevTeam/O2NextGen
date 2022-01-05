bb910c59e7f821333ef2a5799d43766f1104065f
Denis Prokhorchik <live-dev@hotmail.com>
2022-01-05 07:25:19 +0300

    feat(issue-117):  implement the api for versioning (e-sender api)

---
+++ src/Services/e-sender/O2NextGen.ESender.Api/Controllers/VersionController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Reflection;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+
+namespace O2NextGen.ESender.Api.Controllers
+{
+    [AllowAnonymous]
+    public class VersionController : ControllerBase
+    {
+        #region Fields
+
+        private readonly IHostingEnvironment _environment;
+
+        #endregion
+
+
+        #region Ctors
+
+        public VersionController(IHostingEnvironment environment)
+        {
+            _environment = environment;
+        }
+
+        #endregion
+
+        [HttpGet("[controller]")]
+        public object Index()
+        {
+            var exVersion = Assembly.GetExecutingAssembly().GetName().Version;
+            return new
+            {
+                Environment = _environment.EnvironmentName,
+                Version = exVersion.ToString()
+            };
+        }
+    }
+}
+
+
