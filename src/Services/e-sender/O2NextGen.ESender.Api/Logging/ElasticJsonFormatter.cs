3f3eceebdb8409b1abd128d50f57ffd9086bd22e
Denis Prokhorchik <70954129+live-dev999@users.noreply.github.com>
2022-01-09 16:22:51 +0300

    feat(issue-123): update jsonformatter for elk

--- src/Services/e-sender/O2NextGen.ESender.Api/Logging/ElasticJsonFormatter.cs
+++ src/Services/e-sender/O2NextGen.ESender.Api/Logging/ElasticJsonFormatter.cs
@@ -1,4 +1,96 @@
 namespace O2NextGen.ESender.Api.Logging
 {
-    
+    public class ElasticJsonFormatter : ITextFormatter
+	{
+		private const string Prefix = "[ELASTIC]";
+
+		private readonly NamingStrategy namingStrategy;
+		private readonly JsonValueFormatter formatter;
+		private readonly string type;
+
+		public ElasticJsonFormatter(NamingStrategy namingStrategy, JsonValueFormatter formatter, string type)
+		{
+			this.namingStrategy = namingStrategy;
+			this.formatter = formatter;
+			this.type = type;
+		}
+
+		public ElasticJsonFormatter()
+			: this(new CamelCaseNamingStrategy(), new JsonValueFormatter("$type"), "commspoint")
+		{
+		}
+
+		public void Format(LogEvent logEvent, TextWriter output)
+		{
+			output.Write(Prefix);
+			FormatBody(logEvent, output);
+			output.WriteLine();
+		}
+
+		private void FormatBody(LogEvent logEvent, TextWriter output)
+		{
+			// add json body
+			var writer = new JsonTextWriter(output);
+			writer.WriteStartObject();
+
+			// write level
+			writer.WritePropertyName("level");
+			writer.WriteValue(FormatLogLevel(logEvent.Level));
+
+			// write type
+			writer.WritePropertyName("type");
+			writer.WriteValue(type);
+
+			// write timestamp
+			writer.WritePropertyName("timestamp");
+			writer.WriteValue(logEvent.Timestamp.UtcDateTime);
+
+			// write message
+			writer.WritePropertyName("message");
+			writer.WriteValue(logEvent.RenderMessage());
+
+			// write exception if exists
+			if (logEvent.Exception != null)
+			{
+				writer.WritePropertyName("exception");
+				writer.WriteValue(logEvent.Exception.ToString());
+			}
+
+			// write properties
+			writer.WritePropertyName("properties");
+			writer.WriteStartObject();
+			foreach (var property in logEvent.Properties)
+			{
+				var propertyName = namingStrategy.GetPropertyName(property.Key, false);
+				writer.WritePropertyName(propertyName);
+
+				using (var stringWriter = new StringWriter())
+				{
+					formatter.Format(property.Value, stringWriter);
+					writer.WriteRawValue(stringWriter.ToString());
+				}
+			}
+
+			writer.WriteEndObject();
+			writer.Flush();
+		}
+
+		private static string FormatLogLevel(LogEventLevel level)
+		{
+			switch (level)
+			{
+				case LogEventLevel.Verbose:
+					return "TRACE";
+				case LogEventLevel.Debug:
+					return "DEBUG";
+				case LogEventLevel.Warning:
+					return "WARNING";
+				case LogEventLevel.Error:
+				case LogEventLevel.Fatal:
+					return "ERROR";
+				default:
+					return "INFO";
+			}
+		}
+	}
 }
