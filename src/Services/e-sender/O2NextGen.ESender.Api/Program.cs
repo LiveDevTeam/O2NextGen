bb69415354e327af72c32f9a8f8eeeb5a0fb1832
Denis Prokhorchik <live-dev@hotmail.com>
2022-01-09 12:32:08 +0300

    feat(issue-121): update .net core 2.1 to 2.2 & add serilog libs

--- src/Services/e-sender/O2NextGen.ESender.Api/Program.cs
+++ src/Services/e-sender/O2NextGen.ESender.Api/Program.cs
@@ -1,17 +1,52 @@
-﻿using Microsoft.AspNetCore;
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
+using Serilog;
 
 namespace O2NextGen.ESender.Api
 {
     public class Program
     {
-        public static void Main(string[] args)
+        public static readonly string Namespace = typeof(Program).Namespace;
+
+        public static readonly string AppName =
+            Namespace.Substring(Namespace.LastIndexOf('.', Namespace.LastIndexOf('.') - 1) + 1);
+        
+        public static async Task<int> Main(string[] args)
         {
-            CreateWebHostBuilder(args).Build().Run();
+            try
+            {
+                Log.Logger = new LoggerConfiguration()
+                    .Enrich.FromLogContext()
+#if DEBUG
+                    .WriteTo.File("Logs/system_logs.txt")
+#endif
+                    .WriteTo.Console()
+                    
+                    .CreateLogger();
+                
+                var host = CreateWebHostBuilder(args).Build();
+                Log.Information($"############### {AppName} ##############");
+                Log.Information("################# Starting Application #################");
+                await host.RunAsync();
+                Log.Information($"============== {AppName} - state is started =====================");
+                return 0;
+            }
+            catch (Exception ex)
+            {
+                Log.Fatal(ex, "Host terminated unexpectedly");
+                return 1;
+            }
+            finally
+            {
+                Log.CloseAndFlush();
+            }
         }
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
+                .UseSerilog()
                 .UseStartup<Startup>();
     }
 }
