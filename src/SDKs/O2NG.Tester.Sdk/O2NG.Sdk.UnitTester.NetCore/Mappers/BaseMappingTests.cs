333c229ec5ac2aa3c6f2909bc05387f59efa0391
Denis Prokhorchik <live-dev@hotmail.com>
2022-02-14 07:57:23 +0300

    feat(issue-200): add new sdk for test with .net core  support

---
+++ src/SDKs/O2NG.Tester.Sdk/O2NG.Sdk.UnitTester.NetCore/Mappers/BaseMappingTests.cs
@@ -0,0 +1,18 @@
+﻿using NUnit.Framework;
+
+namespace O2NG.Sdk.UnitTester.NetCore.Mappers
+{
+    public class BaseMappingTests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
