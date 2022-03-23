ee6a634367dd769c85d359378581b8c615ca3f30
Denis Prokhorchik <live-dev@hotmail.com>
2022-03-23 23:42:01 +0300

    fix(issue-229): fix the bug of docker-compose

---
+++ src/Services/auth/O2NextGen.Auth.Web/Controllers/VersionController.cs
@@ -0,0 +1,44 @@
+using System;
+using System.Reflection;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace O2NextGen.Auth.Web.Controllers
+{
+
+    [AllowAnonymous]
+    public class VersionController : ControllerBase
+    {
+        #region Fields
+
+        private readonly IHostingEnvironment _environment;
+        private readonly ILogger<VersionController> _logger;
+
+        #endregion
+
+
+        #region Ctors
+
+        public VersionController(IHostingEnvironment environment, ILogger<VersionController> logger)
+        {
+            _environment = environment;
+            _logger = logger;
+        }
+
+        #endregion
+
+        [HttpGet("[controller]")]
+        public object Index()
+        {
+            var exVersion = Assembly.GetExecutingAssembly().GetName().Version;
+            _logger.LogInformation($"get version - {exVersion}");
+            return new
+            {
+                Environment = _environment.EnvironmentName,
+                Version = exVersion.ToString()
+            };
+        }
+    }
+}
