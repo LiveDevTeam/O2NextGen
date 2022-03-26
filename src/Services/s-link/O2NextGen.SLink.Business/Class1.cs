3e29bca4c384677cadb2885071d2332e3c58b3e8
Denis Prokhorchik <live-dev@hotmail.com>
2022-03-26 22:44:46 +0300

    feat(issue-198): add service s-link

---
+++ src/Services/s-link/O2NextGen.SLink.Business/Class1.cs
@@ -0,0 +1,9 @@
+﻿
+using System;
+
+namespace O2NextGen.SLink.Business
+{
+    public class Class1
+    {
+    }
+}
---
+++ src/Services/s-link/O2NextGen.SLink.Impl/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace O2NextGen.SLink.Impl
+{
+    public class Class1
+    {
+    }
+}
