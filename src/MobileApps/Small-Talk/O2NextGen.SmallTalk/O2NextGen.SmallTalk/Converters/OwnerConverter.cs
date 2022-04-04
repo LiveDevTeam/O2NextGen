432d238a3038e94a2154054ecb21eaf632eb536e
Denis Prokhorchik <live-dev@hotmail.com>
2022-04-04 07:03:02 +0300

    feat(issue-285): update ui for chatdetail view

---
+++ src/MobileApps/Small-Talk/O2NextGen.SmallTalk/O2NextGen.SmallTalk/Converters/OwnerConverter.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+using Xamarin.Forms;
+
+namespace O2NextGen.SmallTalk.Core.Converters
+{
+    public class OwnerConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if(value == null)
+                return true;
+            var val = int.Parse(value.ToString());
+            if (val == 1)
+            {
+                return false;
+            }
+            return true;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
