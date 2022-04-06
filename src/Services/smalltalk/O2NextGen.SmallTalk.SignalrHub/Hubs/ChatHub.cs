ca3ac7f1859ac3656fa4b91773416a7cbf933408
Denis Prokhorchik <live-dev@hotmail.com>
2022-04-06 21:13:33 +0300

    feat(issue-291): add hub class for small-talk

---
+++ src/Services/smalltalk/O2NextGen.SmallTalk.SignalrHub/Hubs/ChatHub.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Threading.Tasks;
+
+namespace O2NextGen.SmallTalk.SignalrHub.Hubs
+{
+    public class ChatHub : Hub
+    {
+        public override async Task OnConnectedAsync()
+        {
+            await Groups.AddToGroupAsync(Context.ConnectionId, Context.User.Identity.Name);
+            await base.OnConnectedAsync();
+        }
+
+        public override async Task OnDisconnectedAsync(Exception ex)
+        {
+            await Groups.RemoveFromGroupAsync(Context.ConnectionId, Context.User.Identity.Name);
+            await base.OnDisconnectedAsync(ex);
+        }
+    }
+}
