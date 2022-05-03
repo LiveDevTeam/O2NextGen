ff57a65828fbebaf078af02da0cf15fb69bfb8a2
Denis Prokhorchik <live-dev@hotmail.com>
2022-05-03 22:04:17 +0300

    feat(issue-351): update react client - smalltalk app

--- src/WebApps/smalltalk/auth-app/src/App.jsx
+++ src/WebApps/smalltalk/auth-app/src/App.jsx
@@ -1,14 +1,41 @@
-import React from "react";
+
 import ReactDOM from "react-dom";
 
 import "./index.scss";
+import React, { useEffect } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import SigninOidc from './pages/signin-oidc'
+import SignoutOidc from './pages/signout-oidc'
+import Home from './pages/home'
+import Login from './pages/login'
+import { Provider } from 'react-redux';
+import store from './store';
+import userManager, { loadUserFromStorage } from './services/userService'
+import AuthProvider from './utils/authProvider'
+import PrivateRoute from './utils/protectedRoute'
 
-const App = () => (
-  <div className="mt-10 text-3xl mx-auto max-w-6xl">
-    <div>Name: auth-app</div>
-    <div>Framework: react</div>
-    <div>Language: JavaScript</div>
-    <div>CSS: Tailwind</div>
-  </div>
-);
+function App() {
+
+    useEffect(() => {
+        // fetch current user from cookies
+        loadUserFromStorage(store)
+    }, [])
+
+    return (
+        <Provider store={store}>
+            <AuthProvider userManager={userManager} store={store}>
+                <Router>
+                    <Switch>
+                        <Route path="/login" component={Login} />
+                        <Route path="/signout-oidc" component={SignoutOidc} />
+                        <Route path="/signin-oidc" component={SigninOidc} />
+                        <PrivateRoute exact path="/" component={Home} />
+                    </Switch>
+                </Router>
+            </AuthProvider>
+        </Provider>
+    );
+}
+
+export default App;
 ReactDOM.render(<App />, document.getElementById("app"));
