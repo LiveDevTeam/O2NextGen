1ecfa73c806ae9bf6b39bbd8c58245f7ca35dad6
Denis Prokharhcyk <live-dev@hotmail.com>
2022-09-20 17:23:28 +0300

    feat(issue-420): add terraform files for create resource group

---
+++ deploy/k8s/terraform.tfvars
@@ -0,0 +1,2 @@
+aks_group_name     = "aks-prods"
+aks_group_location = "WestUS3"
---
+++ deploy/k8s/varribles.tf
@@ -0,0 +1,8 @@
+variable "aks_group_name" {
+  type        = string
+  description = "Resourse group for AKS cluster"
+}
+variable "aks_group_location" {
+  type        = string
+  description = "Resourse group location for AKS cluster"
+}
