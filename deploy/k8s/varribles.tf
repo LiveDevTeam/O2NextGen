ee9e98ca36ce4a316d4819860c19d92d1daaab25
Denis Prokharhcyk <live-dev@hotmail.com>
2022-09-20 17:52:07 +0300

    feat(issue-421):  add terraform code for create k8s in azure

--- deploy/k8s/varribles.tf
+++ deploy/k8s/varribles.tf
@@ -2,7 +2,27 @@ variable "aks_group_name" {
   type        = string
   description = "Resourse group for AKS cluster"
 }
+
 variable "aks_group_location" {
   type        = string
   description = "Resourse group location for AKS cluster"
+}
+
+variable "aks_dns_prefix" {
+  type        = string
+  description = "DNS prefix for AKS Cluster"
+}
+
+variable "aks_vm_size" {
+  type        = string
+  description = "Name VM for AKS Cluster"
+}
+
+variable "aks_node_count" {
+  type        = number
+  description = "Node count for AKS Cluster"
+}
+variable "aks_cluster_name" {
+  type        = string
+  description = "AKS Cluster Name"
 }
