ee9c34d5358c57224a9e3e2f327cbedcdde34f9b
Denis Prokharhcyk <live-dev@hotmail.com>
2022-09-23 10:52:40 +0300

    feat(issue-426): add dns zones for k8s in azure

---
+++ deploy/k8s/dns-zones.tf
@@ -0,0 +1,28 @@
+resource "azurerm_dns_zone" "primary" {
+  name                = "o2bus.com"
+  resource_group_name = var.aks_group_name
+
+  tags = {
+    "type_product" = "Saas"
+    "product"      = "O2NextGen Platform"
+  }
+}
+
+# resource "azurerm_dns_a_record" "o2bus_com" {
+#   name                = "www"
+#   zone_name           = azurerm_dns_zone.primary.name
+#   resource_group_name = azurerm_dns_zone.primary.resource_group_name
+#   ttl                 = 300
+#   records             = ["10.0.180.17"] #load balancer ip
+# }
+
+resource "azurerm_dns_zone" "second" {
+  name                = "prf-cent.com"
+  resource_group_name = var.aks_group_name
+
+  tags = {
+    "type"         = "client"
+    "type_product" = "Saas"
+    "product"      = "O2NextGen Platform"
+  }
+}
