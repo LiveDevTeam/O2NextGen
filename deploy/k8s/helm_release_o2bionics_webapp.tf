67469383e6cf3b506b5bc4b444aae60dca11f660
Denis Prokharhcyk <live-dev@hotmail.com>
2022-09-24 19:30:38 +0300

    feat(issue-433): add acr container for azure k8s

--- deploy/k8s/helm_release_o2bionics_webapp.tf
+++ deploy/k8s/helm_release_o2bionics_webapp.tf
@@ -4,9 +4,9 @@
 #   namespace  = "dev"
 #   chart      = "o2bionics-webapp"
 # }
-resource "helm_release" "o2bionicswebappprod" {
-  name       = "o2bionics-webapp"
-  repository = "./charts"
-  namespace  = "prod"
-  chart      = "o2bionics-webapp"
-}
+# resource "helm_release" "o2bionicswebappprod" {
+#   name       = "o2bionics-webapp"
+#   repository = "./charts"
+#   namespace  = "prod"
+#   chart      = "o2bionics-webapp"
+# }
