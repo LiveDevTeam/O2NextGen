195733a4779a05fa6d94b332c6f606d3005fbf7f
Denis Prokharhcyk <live-dev@hotmail.com>
2022-10-03 00:57:37 +0300

    feat(issue-440): add code terraform for deploy infastructure

---
+++ deploy/azure/azure-terraform/main.tf
@@ -0,0 +1,5 @@
+# ========================================== RESOURCE ==========================================
+resource "azurerm_resource_group" "aks-resource-group" {
+  name     = var.k8s_resource_group
+  location = var.k8s_location
+}
---
+++ deploy/azure/azure-terraform/variables.tf
@@ -0,0 +1,9 @@
+variable "k8s_resource_group" {
+  type        = string
+  description = "Resourse group for AKS cluster"
+}
+
+variable "k8s_location" {
+  type        = string
+  description = "Resourse group location for AKS cluster"
+}
