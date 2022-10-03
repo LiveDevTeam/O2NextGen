3f3daaf85e2953e7a21660334fd8548ba98d77ad
Denis Prokharhcyk <live-dev@hotmail.com>
2022-10-04 00:22:11 +0300

    feat(issue-440): update main.tf

--- deploy/release-deploy/clouds/azure/terraform/main.tf
+++ deploy/release-deploy/clouds/azure/terraform/main.tf
@@ -35,13 +35,16 @@ provider "azuread" {
   # subscription_id="f1404c6e-2728-40ae-9cd2-fee75bde4c04"
   tenant_id = "f3a52f65-e3a4-4386-8bc9-a42f32fc1cd6"
 }
-
+# Configure the Microsoft Azure Provider
+provider "azurerm" {
+  features {}
+}
 provider "tls" {}
 # We strongly recommend using the required_providers block to set the
 # Azure Provider source and version being used
 terraform {
-      backend "azurerm" {
-  }
+  #     backend "azurerm" {
+  # }
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
