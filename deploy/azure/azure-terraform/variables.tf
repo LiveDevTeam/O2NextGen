024e1745363f3b02895724dfd938f97b81e808a2
Denis Prokharhcyk <live-dev@hotmail.com>
2022-10-10 22:50:40 +0300

    feat(issue-440): add vars for terraform

--- deploy/azure/azure-terraform/variables.tf
+++ deploy/azure/azure-terraform/variables.tf
@@ -7,3 +7,24 @@ variable "k8s_location" {
   type        = string
   description = "Resourse group location for AKS cluster"
 }
+
+variable "k8s_cluster_name" {
+  type        = string
+  description = "AKS cluster name"
+}
+
+variable "k8s_dns_prefix" {
+  type        = string
+  description = "DNS prefix for AKS Cluster"
+}
+
+variable "k8s_vm_size" {
+  type        = string
+  description = "Name VM for AKS Cluster"
+}
+
+variable "k8s_node_count" {
+  type        = number
+  description = "Node count for AKS Cluster"
+}
+
