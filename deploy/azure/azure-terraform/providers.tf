a7d24a01ca6b7871cc5e8b98b358287acf2fc3ad
Denis Prokharhcyk <live-dev@hotmail.com>
2022-10-10 07:58:45 +0300

    feat(issue-440): update deploy-infastructure-azure-pipelines.yml for azure pipelines

--- deploy/azure/azure-terraform/providers.tf
+++ deploy/azure/azure-terraform/providers.tf
@@ -2,11 +2,17 @@
 provider "azurerm" {
   features {}
 }
-
+provider "kubernetes" {
+  host                   = azurerm_kubernetes_cluster.o2nextgen-aks.kube_config.0.host
+  client_certificate     = base64decode(azurerm_kubernetes_cluster.o2nextgen-aks.kube_config.0.client_certificate)
+  client_key             = base64decode(azurerm_kubernetes_cluster.o2nextgen-aks.kube_config.0.client_key)
+  cluster_ca_certificate = base64decode(azurerm_kubernetes_cluster.o2nextgen-aks.kube_config.0.cluster_ca_certificate)
+}
 terraform {
   backend "azurerm" {
   }
   backend "kubernetes" {
+    
   }
   required_providers {
 
