cc375870d6b7fbc9ffd5b15ada029c7c28ee2dbb
Denis Prokharhcyk <live-dev@hotmail.com>
2022-10-11 00:12:58 +0300

    feat(issue-440): update terraform code and add dns principal for dns zone

--- deploy/azure/azure-terraform/main.tf
+++ deploy/azure/azure-terraform/main.tf
@@ -75,3 +75,76 @@ resource "azurerm_dns_zone" "primary-dns-zone" {
 }
 
 
+# current subscription
+data "azurerm_subscription" "current" {}
+
+# # current client
+data "azuread_client_config" "current" {}
+
+output "current_subscription_display_name" {
+  value = data.azurerm_subscription.current.display_name
+}
+
+output "object_id" {
+  value = data.azuread_client_config.current.object_id
+}
+
+# Create an application
+resource "azuread_application" "example" {
+  depends_on = [
+    azurerm_dns_zone.primary-dns-zone
+  ]
+  display_name = "external-dns"
+  owners       = [data.azuread_client_config.current.object_id]
+}
+# Create Service Principal linked to the Application
+resource "azuread_service_principal" "current" {
+  application_id               = azuread_application.example.application_id
+  app_role_assignment_required = false
+  owners                       = [data.azuread_client_config.current.object_id]
+}
+
+resource "azuread_application_password" "current" {
+  display_name          = "rbac"
+  application_object_id = azuread_application.example.object_id
+}
+
+# Create role assignment for service principal
+# https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/resources/role_assignment
+resource "azurerm_role_assignment" "current" {
+  scope                = data.azurerm_subscription.current.id
+  role_definition_name = "Contributor"
+
+  # When assigning to a SP, use the object_id, not the appId
+  # see: https://docs.microsoft.com/en-us/azure/role-based-access-control/role-assignments-cli
+  principal_id = azuread_service_principal.current.object_id
+}
+
+# Create role assignment for service principal
+resource "azurerm_role_assignment" "main" {
+  scope                = azurerm_dns_zone.primary-dns-zone.id
+  role_definition_name = "DNS Zone Contributor"
+  principal_id         = azuread_service_principal.current.object_id
+}
+
+# Create role assignment for service principal
+resource "azurerm_role_assignment" "reader" {
+  scope                = azurerm_resource_group.aks-resource-group.id
+  role_definition_name = "Reader"
+  principal_id         = azuread_service_principal.current.object_id
+}
+
+output "display_name" {
+  value = azuread_service_principal.current.display_name
+}
+
+output "client_id" {
+  value = azuread_application.example.application_id
+}
+
+output "client_secret" {
+  value     = azuread_application_password.current.value
+  sensitive = true
+}
+
+
