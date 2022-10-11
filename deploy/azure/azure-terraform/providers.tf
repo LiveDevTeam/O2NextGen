a5b746fecb182d62c1ea1d448dcf8a2305b03b0b
Denis Prokharhcyk <live-dev@hotmail.com>
2022-10-11 17:12:22 +0300

    feat(issue-440): update version azuread provider

--- deploy/azure/azure-terraform/providers.tf
+++ deploy/azure/azure-terraform/providers.tf
@@ -16,10 +16,10 @@ provider "helm" {
     cluster_ca_certificate = base64decode(azurerm_kubernetes_cluster.o2nextgen-aks.kube_config.0.cluster_ca_certificate)
   }
 }
-# Retrieve domain information
-data "azuread_domains" "example" {
-  only_initial = true
-}
+# # Retrieve domain information
+# data "azuread_domains" "example" {
+#   only_initial = true
+# }
 
 # Configure the Azure Active Directory Provider
 provider "azuread" {
