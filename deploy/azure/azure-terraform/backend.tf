ae0b3a2cb7cdc67489f49a42f80da836b46e458d
Denis Prokharhcyk <live-dev@hotmail.com>
2022-10-12 10:22:31 +0300

    feat(issue-440): update providers

--- deploy/azure/azure-terraform/backend.tf
+++ deploy/azure/azure-terraform/backend.tf
@@ -1,5 +1,8 @@
 terraform {
   backend "azurerm" {
-    
+    resource_group_name  = "devops-tfstate"
+    storage_account_name = "terraformstate999"
+    container_name       = "tfstate"
+    key                  = "terraform.tfstate"
   }
 }
