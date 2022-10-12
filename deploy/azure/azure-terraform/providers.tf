f136a8b54140d067bd5985d8f1e3abc5697556cd
Denis Prokharhcyk <live-dev@hotmail.com>
2022-10-12 11:37:23 +0300

    feat(issue-440): update providers.tf

--- deploy/azure/azure-terraform/providers.tf
+++ deploy/azure/azure-terraform/providers.tf
@@ -1,3 +1,14 @@
+# Configure the Azure Active Directory Provider
+provider "azuread" {
+  # subscription_id="f1404c6e-2728-40ae-9cd2-fee75bde4c04"
+  tenant_id = "f3a52f65-e3a4-4386-8bc9-a42f32fc1cd6"
+}
+
+# Configure the Microsoft Azure Provider
+provider "azurerm" {
+  features {}
+}
+
 # We strongly recommend using the required_providers block to set the
 # Azure Provider source and version being used
 terraform {
@@ -6,11 +17,11 @@ terraform {
       source  = "hashicorp/azurerm"
       version = "=3.0.0"
     }
+    azuread = {
+      source  = "hashicorp/azuread"
+      version = "~> 2.15.0"
+    }
   }
 }
 
-# Configure the Microsoft Azure Provider
-provider "azurerm" {
-  features {}
-  environment = public
-}
+
