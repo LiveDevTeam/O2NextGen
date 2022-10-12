0f6b2a5b2ce9af8705e5ada62bc369c3a35385a0
Denis Prokharhcyk <live-dev@hotmail.com>
2022-10-12 09:42:41 +0300

    feat(issue-440): update auto-terraform code

--- deploy/azure/azure-terraform/backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "azurerm" {
-    resource_group_name  = "devops-tfstate"
-    storage_account_name = "terraformstate999"
-    container_name       = "tfstate"
-    key                  = "terraform.tfstate"
-  }
-}
