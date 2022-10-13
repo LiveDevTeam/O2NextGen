22e2526050d240cede8622c3796fc234418c0bf3
Denis Prokharhcyk <live-dev@hotmail.com>
2022-10-13 22:27:13 +0300

    feat(issue-440): update backend.tf

--- deploy/azure/azure-terraform/backend.tf
+++ deploy/azure/azure-terraform/backend.tf
@@ -1,5 +1,5 @@
 terraform {
-  backend "azurerm" {
+  backend "azure" {
     # resource_group_name  = "devops-tfstate"
     # storage_account_name = "terraformstate999"
     # container_name       = "tfstate"
