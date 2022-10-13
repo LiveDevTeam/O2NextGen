2a31861a8ee044573ae1adc502c26a8b3a0a3af9
Denis Prokharhcyk <live-dev@hotmail.com>
2022-10-13 22:42:46 +0300

    feat(issue-440): update vars for terraform

--- deploy/azure/azure-terraform/variables.tf
+++ deploy/azure/azure-terraform/variables.tf
@@ -5,5 +5,35 @@ variable "k8s_resource_group" {
 
 variable "k8s_location" {
   type        = string
-  description = "Location for AKS cluster"
+  description = "Resourse group location for AKS cluster"
+}
+variable "k8s_cluster_name" {
+  type        = string
+  description = "AKS cluster name"
+}
+
+variable "k8s_dns_prefix" {
+  type        = string
+  description = "DNS prefix for AKS Cluster"
+}
+
+variable "k8s_vm_size" {
+  type        = string
+  description = "Name VM for AKS Cluster"
+}
+
+
+variable "k8s_node_count" {
+  type        = number
+  description = "Node count for AKS Cluster"
+}
+
+variable "grafana_admin_user" {
+  type        = string
+  description = "Admin user to access Grafana dashboard"
+}
+
+variable "grafana_admin_password" {
+  type        = string
+  description = "Admin password to access Grafana dashboard"
 }
