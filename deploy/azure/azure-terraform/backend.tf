58fdb97e3a499b8f6185ac3d911c45a05ae6fcab
Denis Prokharhcyk <live-dev@hotmail.com>
2022-10-13 22:30:59 +0300

    feat(issue-440): update pipeline

--- deploy/azure/azure-terraform/backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "azure" {
-    # resource_group_name  = "devops-tfstate"
-    # storage_account_name = "terraformstate999"
-    # container_name       = "tfstate"
-    # key                  = "terraform.tfstate"
-  }
-}
--- deploy/azure/azure-terraform/providers.tf
+++ deploy/azure/azure-terraform/providers.tf
@@ -1,7 +1,7 @@
 # Configure the Azure Active Directory Provider
 provider "azuread" {
   # subscription_id="f1404c6e-2728-40ae-9cd2-fee75bde4c04"
-  tenant_id = "f3a52f65-e3a4-4386-8bc9-a42f32fc1cd6"
+  # tenant_id = "f3a52f65-e3a4-4386-8bc9-a42f32fc1cd6"
 }
 
 # Configure the Microsoft Azure Provider
@@ -12,6 +12,12 @@ provider "azurerm" {
 # We strongly recommend using the required_providers block to set the
 # Azure Provider source and version being used
 terraform {
+   backend "azure" {
+    # resource_group_name  = "devops-tfstate"
+    # storage_account_name = "terraformstate999"
+    # container_name       = "tfstate"
+    # key                  = "terraform.tfstate"
+  }
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
