5c616247c24fa761253e19860819f0b7f25113e2
Denis Prokharchyk <live-dev@hotmail.com>
2022-12-10 12:54:08 +0300

    feat: add keyvault in terraform

--- deploy/azure/azure-terraform/main.tf
+++ deploy/azure/azure-terraform/main.tf
@@ -240,6 +240,45 @@ resource "azuread_application_password" "current" {
   display_name          = "rbac"
   application_object_id = azuread_application.example.object_id
 }
+
+resource "azurerm_key_vault" "keyvault" {
+  name                        = var.keyvault_name
+  location                    = var.location
+  resource_group_name         = azurerm_resource_group.rg.name
+  enabled_for_disk_encryption = false
+  tenant_id                   = data.azurerm_client_config.current.tenant_id
+  soft_delete_enabled         = true
+  soft_delete_retention_days  = 7
+  purge_protection_enabled    = false
+
+  sku_name = "standard"
+
+  access_policy {
+    tenant_id = data.azurerm_client_config.current.tenant_id
+    object_id = data.azurerm_client_config.current.object_id
+
+    key_permissions = [
+      "get",
+    ]
+
+    secret_permissions = [
+      "get",
+      "list",
+      "set",
+      "delete"
+    ]
+
+    storage_permissions = [
+      "get",
+    ]
+  }
+
+  # network_acls {
+  #   default_action = "Deny" # "Allow" 
+  #   bypass         = "AzureServices" # "None"
+  #   ip_rules = ["50.50.50.50/24"]
+  # }
+}
 # resource "time_rotating" "example" {
 #   rotation_days = 7
 # }
