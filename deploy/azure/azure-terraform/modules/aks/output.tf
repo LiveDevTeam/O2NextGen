37e523b79cb906c52b568096194205193e9de22c
Denis Prokharchyk <live-dev@hotmail.com>
2023-01-22 00:10:59 +0300

    deploy(terraform): add acr to azure

--- deploy/azure/azure-terraform/modules/aks/output.tf
+++ deploy/azure/azure-terraform/modules/aks/output.tf
@@ -19,7 +19,10 @@ output "k8s_resource_group_id" {
 output "k8s_cluster_id" {
   value = azurerm_kubernetes_cluster.o2nextgen-aks.kubelet_identity.0.object_id
 }
-# host                   = azurerm_kubernetes_cluster.o2nextgen-aks.kube_config.0.host
-#     client_certificate     = base64decode(azurerm_kubernetes_cluster.o2nextgen-aks.kube_config.0.client_certificate)
-#     client_key             = base64decode(azurerm_kubernetes_cluster.o2nextgen-aks.kube_config.0.client_key)
-#     cluster_ca_certificate = base64decode(azurerm_kubernetes_cluster.o2nextgen-aks.kube_config.0.cluster_ca_certificate)
+
+provider "kubernetes" {
+  host                   = azurerm_kubernetes_cluster.o2nextgen-aks.kube_config.0.host
+  client_certificate     = base64decode(azurerm_kubernetes_cluster.o2nextgen-aks.kube_config.0.client_certificate)
+  client_key             = base64decode(azurerm_kubernetes_cluster.o2nextgen-aks.kube_config.0.client_key)
+  cluster_ca_certificate = base64decode(azurerm_kubernetes_cluster.o2nextgen-aks.kube_config.0.cluster_ca_certificate)
+}
