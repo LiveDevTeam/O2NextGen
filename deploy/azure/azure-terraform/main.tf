ba73d10e671db8477d401c0aa5ee01ea461d03d3
Denis Prokharchyk <live-dev@hotmail.com>
2023-01-31 18:01:11 +0300

    deploy(terraform): update envs storage blob

--- deploy/azure/azure-terraform/main.tf
+++ deploy/azure/azure-terraform/main.tf
@@ -703,7 +703,7 @@ resource "azurerm_storage_account" "storage" {
 
 resource "azurerm_storage_container" "container" {
   name                  = var.storage_container_name
-  storage_account_name  = azurerm_storage_account.storage.name
+  storage_account_name  = var.storage_account_name
   container_access_type = "container" # "blob" "private"
 }
 
