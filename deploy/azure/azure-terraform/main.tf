a7ac8e92331d6617b775a5cba50dfd6cd21b41d8
Denis Prokharchyk <live-dev@hotmail.com>
2023-01-31 18:49:33 +0300

    deploy(terraform): add  storage blob

--- deploy/azure/azure-terraform/main.tf
+++ deploy/azure/azure-terraform/main.tf
@@ -29,8 +29,8 @@ provider "tls" {}
 # We strongly recommend using the required_providers block to set the
 # Azure Provider source and version being used
 terraform {
-  # backend "azure" {
-  # }
+  backend "azure" {
+  }
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
