ca38fd3acbbd7d89035eaa2f4013af830393ccc7
Denis Prokharchyk <live-dev@hotmail.com>
2023-01-31 14:53:49 +0300

    feat(issue-515): add certs for idserver

---
+++ src/Services/o2bionics-id/src/O2Bionics.Services.IdServer/Certificates/GenerateCertificateOptions.cs
@@ -0,0 +1,65 @@
+using System.Security.Cryptography;
+using System.Security.Cryptography.X509Certificates;
+
+namespace O2Bionics.Services.IdServer.Certificates;
+
+public class certificateHelper
+{
+    public static void MakeCert(GenerateCertificateOptions options)
+    {
+        var rsa = RSA.Create(4096);
+        var req = new CertificateRequest($"cn={options.CommonName}", rsa, HashAlgorithmName.SHA256, RSASignaturePadding.Pkcs1);
+        var cert = req.CreateSelfSigned(DateTimeOffset.Now, DateTimeOffset.Now.AddYears(options.Years));
+        var path = Path.Combine(options.PathToSave, options.CertificateFileName);
+
+        // Create PFX (PKCS #12) with private key
+        File.WriteAllBytes($"{path}.pfx", cert.Export(X509ContentType.Pfx, options.Password));
+
+        // Create Base 64 encoded CER (public key only)
+        File.WriteAllText($"{path}.cer",
+            "-----BEGIN CERTIFICATE-----\r\n"
+            + Convert.ToBase64String(cert.Export(X509ContentType.Cert), Base64FormattingOptions.InsertLineBreaks)
+            + "\r\n-----END CERTIFICATE-----");
+    }
+}
+public class GenerateCertificateOptions
+{
+    public GenerateCertificateOptions(string pathToSave, string commonName, string fileName, string password, int years = 1)
+    {
+        CommonName = commonName;
+        if (string.IsNullOrEmpty(CommonName))
+        {
+            throw new ArgumentNullException(nameof(CommonName));
+        }
+
+        PathToSave = pathToSave;
+        if (string.IsNullOrEmpty(PathToSave))
+        {
+            throw new ArgumentNullException(nameof(PathToSave));
+        }
+
+        Password = password;
+        if (string.IsNullOrEmpty(Password))
+        {
+            throw new ArgumentNullException(nameof(Password));
+        }
+
+        CertificateFileName = fileName;
+        if (string.IsNullOrEmpty(CertificateFileName))
+        {
+            throw new ArgumentNullException(nameof(CertificateFileName));
+        }
+
+        Years = years;
+        if (Years <= 0)
+        {
+            Years = 1;
+        }
+    }
+
+    public string CommonName { get; }
+    public string PathToSave { get; }
+    public string Password { get; }
+    public string CertificateFileName { get; }
+    public int Years { get; }
+}
--- src/Services/o2bionics-id/src/O2Bionics.Services.IdServer/Program.cs
+++ src/Services/o2bionics-id/src/O2Bionics.Services.IdServer/Program.cs
@@ -1,7 +1,10 @@
+using System.Security.Cryptography.X509Certificates;
 using Duende.IdentityServer.Services;
+using Microsoft.AspNetCore.DataProtection;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using O2Bionics.Services.IdServer;
+using O2Bionics.Services.IdServer.Certificates;
 using O2Bionics.Services.IdServer.DbContexts;
 using O2Bionics.Services.IdServer.Initializer;
 using O2Bionics.Services.IdServer.Models;
@@ -17,7 +20,29 @@ builder.Services.AddDbContext<ApplicationDbContext>(options =>
 builder.Services.AddIdentity<ApplicationUser, IdentityRole>()
     .AddEntityFrameworkStores<ApplicationDbContext>()
     .AddDefaultTokenProviders();
+// var dataProtectionKeysLocation =
+//     builder.Configuration.GetSection<DataProtectionSettings>(nameof(DataProtectionSettings))?.Location;
+// if (!string.IsNullOrWhiteSpace(dataProtectionKeysLocation))
+{
+    // builder.Services
+    //     .AddDataProtection();
+    // .PersistKeysToFileSystem(new DirectoryInfo(dataProtectionKeysLocation));
+    // TODO: encrypt the keys
+}
+var pass = "P@55w0rd";
+var filename = "IdentityServer4_certificate";
+var filepath = Path.Combine(builder.Environment.ContentRootPath, filename );
+// var options = new GenerateCertificateOptions
+// (
+//     pathToSave: builder.Environment.ContentRootPath,
+//     commonName: "IdentityServer4",
+//     fileName: filename,
+//     password: pass,
+//     5
+// );
+// certificateHelper.MakeCert(options);
 
+var certificate = new X509Certificate2(Path.Combine(builder.Environment.ContentRootPath,"IdentityServer4_certificate.pfx"), pass);
 builder.Services.AddIdentityServer(options =>
     {
         options.Events.RaiseErrorEvents = true;
@@ -30,7 +55,8 @@ builder.Services.AddIdentityServer(options =>
     .AddInMemoryApiScopes(SD.ApiScopes)
     .AddInMemoryClients(SD.GetClients(SD.GetUrls(builder.Configuration)))
     .AddAspNetIdentity<ApplicationUser>()
-    .AddDeveloperSigningCredential();
+    .AddSigningCredential(certificate);
+    //.AddDeveloperSigningCredential();
 builder.Services.AddScoped<IDbInitializer, DbInitializer>();
 builder.Services.AddScoped<IProfileService,ProfileService>();
 builder.Services.AddControllersWithViews();
