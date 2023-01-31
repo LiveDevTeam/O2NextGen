ef495e3887e049368c2b8105e9218cd0249c7449
Denis Prokharchyk <live-dev@hotmail.com>
2023-01-31 20:31:19 +0300

    deploy: fix https pack idserver

--- src/Services/o2bionics-id/src/O2Bionics.Services.IdServer/Program.cs
+++ src/Services/o2bionics-id/src/O2Bionics.Services.IdServer/Program.cs
@@ -105,6 +105,7 @@ if (!app.Environment.IsDevelopment())
 }
 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
 app.UseHsts();
+app.Use((context, next) => { context.Request.Scheme = "https"; return next(); });
 app.UseHttpsRedirection();
 app.UseStaticFiles();
 using var scope = app.Services.CreateScope();
