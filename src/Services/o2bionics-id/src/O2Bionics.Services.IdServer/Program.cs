ebaa462802808d7617dfcf29a5b9a4de41b8fc19
Denis Prokharchyk <live-dev@hotmail.com>
2023-01-31 21:22:10 +0300

    deploy: fix https for pfr-mvc and idserver

--- src/Services/o2bionics-id/src/O2Bionics.Services.IdServer/Program.cs
+++ src/Services/o2bionics-id/src/O2Bionics.Services.IdServer/Program.cs
@@ -105,6 +105,7 @@ if (!app.Environment.IsDevelopment())
 }
 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
 app.UseHsts();
+app.UseHttpsRedirection();
 app.Use((context, next) => { context.Request.Scheme = "https"; return next(); });
 app.UseHttpsRedirection();
 app.UseStaticFiles();
@@ -114,7 +115,6 @@ initializer?.Initialize();
 app.UseRouting();
 app.UseIdentityServer();
 app.UseAuthorization();
-
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
--- src/WebApps/PFRCentr.Mvc/src/PFRCentr.App.MvcClient/Program.cs
+++ src/WebApps/PFRCentr.Mvc/src/PFRCentr.App.MvcClient/Program.cs
@@ -60,6 +60,7 @@ if (!app.Environment.IsDevelopment())
 }
 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
 app.UseHsts();
+app.UseHttpsRedirection();
 //fix https://github.com/IdentityServer/IdentityServer4/issues/4645
 app.Use((context, next) => { context.Request.Scheme = "https"; return next(); });
 app.UseHttpsRedirection();
