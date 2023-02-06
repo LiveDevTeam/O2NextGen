78d958987bf1f7df90a4a7d9fdd0d9ef1c2454bc
Denis Prokharhcyk <live-dev@hotmail.com>
2023-02-06 08:08:06 +0300

    feat(issue-536): add tests for idportal service

---
+++ src/Services/id-portal/tests/Tests.O2Bionics.Services.IdPortal/StubVersionController.cs
@@ -0,0 +1,22 @@
+using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Logging;
+using O2Bionics.Services.IdPortal.Controllers;
+
+namespace Tests.O2Bionics.Services.IdPortal;
+
+public class StubVersionController : VersionController
+{
+    public string VersionString { get; init; }
+
+#pragma warning disable CS8618
+    public StubVersionController(IWebHostEnvironment environment,
+#pragma warning restore CS8618
+        // ReSharper disable once ContextualLoggerProblem
+        ILogger<VersionController> logger)
+        : base(environment, logger)
+    {
+    }
+
+    protected override Version GetVersion() => Version.Parse(VersionString);
+}
