6f8be5bb67d332ce44da206da8fd8114f93a07e9
Denis Prokharhcyk <live-dev@hotmail.com>
2023-02-06 23:59:14 +0300

    feat(issue-538): add idportal service

---
+++ src/Services/id-portal/src/O2Bionics.Services.IdPortal/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace O2Bionics.Services.IdServer.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
