a07cdcc760d1c18a77300838f49f4c4d7b592a45
Denis Prokharchyk <live-dev@hotmail.com>
2023-02-07 09:29:49 +0300

    fix(538): add docke for idportal mvc

--- src/WebApps/IdPortal.Mvc/src/PFRCentr.Mvc.App/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace PFRCentr.Mvc.App.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
