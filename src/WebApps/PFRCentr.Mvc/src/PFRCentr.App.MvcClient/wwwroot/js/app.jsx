bd7d2d47848d917c2ea7d24bc09d40d792531a12
Denis Prokharchyk <live-dev@hotmail.com>
2023-02-08 22:35:05 +0300

    #542:  include reactjs to mvc

---
+++ src/WebApps/PFRCentr.Mvc/src/PFRCentr.App.MvcClient/wwwroot/js/app.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import * as ReactDOM from 'react-dom';
+
+class Hello extends React.Component {
+    render() {
+        return <h1>Привет, React.JS</h1>;
+    }
+}
+ReactDOM.render(
+    <Hello />,
+    document.getElementById("content")
+);
